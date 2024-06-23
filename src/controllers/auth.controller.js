6058df6b5fdfdb09c9e458bd666bd8b53b642316
Christopher Ortega <chriiswho@gmail.com>
2024-06-22 20:21:08 -0700

    Login/logout and destroy access token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -69,6 +69,55 @@ export const authRegister = async (req, res) =>{
 
 export const authLogin = async (req, res) =>{
 
-    res.send('Login');
+    try{
+
+        const { email, password} = req.body;
+        const userFound = await User.findOne({ email });
+
+        if(!userFound) 
+            return res.status(400).json({
+                
+                message: ['The email does not exist.'],
+
+            });
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        
+        if (!isMatch) 
+            
+            return res.status(400).json({
+
+            message: ["The password is incorrect"],
+
+              });
+
+        const token = await createAccessToken({
+            id: userFound._id,
+            username: userFound.username,
+        });
+
+        res.cookie('token', token);
+
+        res.json({
+            id: userFound._id,
+            username: userFound.username,
+            email: userFound.email,
+        });
+
+    }
+    catch(error){
+        return res.status(500).json({message: error.message});
+    }
     
+
+}
+
+export const authLogout = async (req, res) => {
+
+    res.cookie('token', '', {
+        expires: new Date(0),
+    })
+
+    return res.sendStatus(200);
+
 }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { authRegister, authLogin } from "../controllers/auth.controller.js";
+import { authRegister, authLogin, authLogout } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post('/register', authRegister);
 router.post('/login', authLogin);
+router.post('/logout', authLogout);
 
 export default router;
